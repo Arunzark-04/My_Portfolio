7b78ec7c690042d33e1cb38a9d947b8385cdb1ff
Arunzark-04 <115612108+Arunzark-04@users.noreply.github.com>
2023-09-12 09:41:54 +0530

    Add files via upload

---
+++ Portfolio_website/Resources/js/main.js
@@ -0,0 +1,8 @@
+<script>
+    function submitForm() {
+        // Here, you can add your code to handle form submission, such as sending an email or storing data.
+        
+        // Display a confirmation message to the user.
+        document.getElementById("confirmationMessage").innerHTML = "Your response has been submitted. Thank you!"
+}
+</script>
